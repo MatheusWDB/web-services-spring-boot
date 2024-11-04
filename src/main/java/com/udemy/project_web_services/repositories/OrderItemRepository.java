c770105aa6dcbb5146ffe42c737eeb7d8abe9f56
MatheusWDB <matheuswdb1703@gmail.com>
2024-11-04 13:45:12 -0300

    Mudanças

--- src/main/java/com/udemy/project_web_services/repositories/OrderItemRepository.java
+++ src/main/java/com/udemy/project_web_services/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.udemy.project_web_services.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.udemy.project_web_services.entities.OrderItem;
+import com.udemy.project_web_services.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
     
 }
